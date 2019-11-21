7ca4f2d335f56f33800dece07d3566406907a788
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2019-11-21 11:44:14 +0100

    v0.9.11

--- et_micc_build/__init__.py
+++ et_micc_build/__init__.py
@@ -7,5 +7,8 @@ Package et_micc_build
 Top-level package for et_micc_build.
 """
 import et_micc_build.cli_micc_build
+import et_micc
 
-__version__ = "0.2.7"
+# et-micc and et--micc-build have identical version string, although
+# they are different packages. 
+__version__ = et_micc.__version__
